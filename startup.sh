9c5e3e90de7bee33f441bf4bfce75214a62d60bf
Tom Boone <tbc@tomboone.com>
2025-09-03 18:19:31 -0400

    add migration and cache clear to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,18 @@
+#!/bin/bash
+set -e
+
+# Copy nginx config
 cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-enabled/default
 nginx -s reload
+
+# Navigate to BookStack directory
+cd /home/site/wwwroot/BookStack
+
+# Wait for database to be ready (optional but recommended)
+sleep 5
+
+# Run database migrations
+php artisan migrate --force
+
+# Clear caches
+php artisan optimize:clear
